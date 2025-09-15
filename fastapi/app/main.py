b39ea9c8d2df1efd5f0ad731c07eb4a948af8863
= <=>
2025-09-15 15:06:05 +0300

    Added secrets test

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from pathlib import Path
+from os import environ
 
 app = FastAPI()
 
 
 @app.get("/")
 def hello_world():
-    return {"message": "OK"}
+    return {"message": "OK!"}
+
+@app.get("/password")
+def get_password():
+    password_file = environ.get("PASSWORD_FILE")
+    secret_path = Path(password_file)
+    password = secret_path.read_text().strip() if secret_path.exists() else None
+    return { "password": password }
+
+@app.get("/token")
+def get_token():
+    token_file = environ.get("TOKEN_FILE")
+    secret_path = Path(token_file)
+    token = secret_path.read_text().strip() if secret_path.exists() else None
+    return { "token": token }
